fe50104afddbbe4d4dc47bea83cb7b172ffd0648
Henry Bust <Henry.Bust@hotmail.de>
2022-09-02 22:12:41 +0200

    yeet

--- dbConnector.py
+++ dbConnector.py
@@ -4,8 +4,9 @@ This Class is responsible for handling the Database Interactions
 It uses sqlite3 to store and retrieve Data that will later be used
 by the *Insert use here*
 """
+from msilib.schema import Error
 import sqlite3
-import datetime
+import traceback
 
 # TODO: Connect to real Log-File
 def errorlog(errorstring):
@@ -25,21 +26,19 @@ class dbConnector():
         c = self.conn.cursor()
         command = """
         CREATE TABLE IF NOT EXISTS Ingredients (
-            ID INT NOT NULL AUTO_INCREMENT,
-            name VARCHAR(255) NOT NULL UNIQUE,
-            PRIMARY KEY(ID)
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL UNIQUE
         );
 
         CREATE TABLE IF NOT EXISTS Recipes (
-            ID INT NOT NULL AUTO_INCREMENT,
-            name VARCHAR(255) NOT NULL,
-            source VARCHAR(255) UNIQUE,
-            img_source VARCHAR(255),
-            rating FLOAT,
-            PRIMARY KEY(ID)
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            source TEXT UNIQUE,
+            img_source TEXT,
+            rating FLOAT
         );
 
-        CREATE TABLE IF NOT EXISTS recipes_ingredients (
+        CREATE TABLE IF NOT EXISTS Recipes_Ingredients (
             recipe_id INT NOT NULL,
             ingredient_id INT NOT NULL
         );
@@ -51,11 +50,78 @@ class dbConnector():
 
     def close_connection(self):
         self.conn.close()
+    
+    def addRecipe(self, Name: str, Source: str, Img_Source: str, rating: float, ingredients: list):
+        try:
+            try:
+                c = self.conn.cursor()
+                tpl = (Name, Source, Img_Source, rating)
+                c.execute("""
+                INSERT INTO Recipes
+                (name, source, img_source, rating)
+                VALUES (?,?,?,?)
+                """, tpl)
+                c.close()
+                self.conn.commit()
+            except sqlite3.IntegrityError:
+                    print("INFO: Failed to add Recipe (source must be unique)")
+            c.execute("""
+            SELECT ID from Recipes
+            WHERE name = '{}'""".format(Name))
+            recipeID = c.fetchall()[0][0]
+            for ingredient in ingredients:
+                try:
+                    c.execute("INSERT INTO Ingredients(name) VALUES('" + ingredient + "')")
+                    self.conn.commit()
+                except sqlite3.IntegrityError:
+                    print("INFO: Failed to add Ingredient (name must be unique)")
+                except Exception as e:
+                    pass
+                finally:
+                    c.execute("SELECT ID FROM Ingredients WHERE name ='" + ingredient + "'")
+                    ingredientID = c.fetchall()[0][0]
+                    c.execute("INSERT INTO Recipes_Ingredients(recipe_id, ingredient_id) VALUES ({},{})".format(str(recipeID),str(ingredientID)))
+                    self.conn.commit()
+        except:
+            traceback.print_exc
+        finally:
+            c.close()
+        
+    def getRecipes(self, ingredients: list):
+        c = self.conn.cursor()
+        ingredient_ids = []
+        for ingredient in ingredients:
+            SQL = """
+            SELECT ID
+            FROM Ingredients
+            WHERE name = {}""".format(ingredient)
+
+    def addIngredient(self, Name: str):
+        try:    
+            c = self.conn.cursor()
+            c.execute("""
+            INSERT INTO Ingredients
+            (name)
+            VALUES ('{}')
+            """.format(Name.lower()))
+            c.close()
+            self.conn.commit()
+        except sqlite3.IntegrityError:
+            print("INFO: Failed to add Ingredient (Ingredient name must be unique)")
+        except sqlite3.OperationalError:
+            print("ERROR: operational error")
+        except:
+            print("ERROR: addIngredient failed: Unknown error")
+    
+    def addIngredients(self, names: list):
+        for name in names:
+            self.addIngredient(name)
 
 
 def main():
     connection = dbConnector()
     connection.createDatabase()
+    connection.addRecipe("qqq", "asdas", "dmdm", 4.5, ["banane", "salat"])
     connection.close_connection()
     print("Done")
 
