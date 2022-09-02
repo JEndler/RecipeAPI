80b95086418fb242f39f7c8808ae5f222426ea4b
Jakob Endler <jakob.endler@hotmail.de>
2022-09-02 19:27:23 +0200

    removed some files and fixed scraper partially

--- RezeptScraper.py
+++ RezeptScraper.py
@@ -6,12 +6,33 @@ import random
 from datetime import datetime
 from multiprocessing import pool
 from recipeManager import addRecipe
+import logging
+
+"""
+@Author: Jakob Endler
+"""
+
+
+# Instantiating the Logger and the File Handler
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('data/log.txt')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 CHEFKOCH_LINK = "https://www.chefkoch.de/rs/s0/Rezepte.html"
 
 SAMPLE_RECIPE = "https://www.chefkoch.de/rezepte/2293561365672708/Gulaschsuppe-im-Kessel-oder-Topf.html"
 
 def getRawData(url, useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'):
+	"""returns a bs4.soup-Object of the given url
+    @Params: 
+        url: a string-url for a HLTV-Match page
+		useragent: a string-useragent for the request
+    @returns a bs4.soup-Object
+	"""
 
 	# User Agent Mozilla to Circumvent Security Blocking
 	req = Request(url, headers={'User-Agent': useragent})
@@ -26,8 +47,14 @@ def getRawData(url, useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67
 	return page_soup
 
 def findRezeptLinks(page_soup):
+	"""returns a list of all links to recipes on the given page
+
+	@Params:
+		page_soup: a bs4.soup-Object of a recipe page
+	@returns a list of links as strings
+	"""
 	rezept_link_list = []
-	result_holder = page_soup.findAll("article", {"class": "rsel-item ds-grid-float ds-col-12 ds-col-m-8"})
+	result_holder = page_soup.findAll("div", {"class": "ds-recipe-card bi-recipe-item"})
 	result = []
 	for rezept in result_holder:
 		result.extend(rezept.findAll("a", {'href': True}))
@@ -38,27 +65,30 @@ def findRezeptLinks(page_soup):
 	return rezept_link_list
 
 def findLinkToNextPage(page_soup):
-	nextPage = page_soup.find("a", {"class": "ds-page-link bi-paging-next"})
+	nextPage = page_soup.find("a", {"class": "ds-btn ds-btn--round ds-btn--control"})
 	return nextPage["href"]
 
 def constructAllRecipePageLinks(count = 11420):
 	basehtml = "https://www.chefkoch.de/rs/s__/Rezepte.html"
 	result = [CHEFKOCH_LINK]
 	for i in range(count):
-		result.append(basehtml.replace("__", str(i*30)))
+		result.append(basehtml.replace("__", str(i)))
 	return result
 
 def getAllRecipePageLinks():
-	#Weil insgesamt ~340k Rezepte und 30 pro Page => 11420
-	recipePageCount = 11420
+	#Weil insgesamt ~360k Rezepte und 41 pro Page => 11420
 	result = []
 	currentlink = CHEFKOCH_LINK
 	while  True:
-		if len(result) > recipePageCount:
-			return result
 		if len(result) % 100 == 0: print("RecipePages Loaded: " + str(len(result)))
 		result.append(currentlink)
-		currentlink = findLinkToNextPage(getRawData(currentlink))
+		try:
+			currentlink = "https://www.chefkoch.de" + findLinkToNextPage(getRawData(currentlink))
+		except Exception as e:
+			print("Error while loading next page")
+			print(e)
+			break
+	return result
 
 def asyncLoad(recipePageList):
 	with pool.Pool(4) as p:
@@ -111,7 +141,13 @@ def downloadStuff():
 
 startTime = datetime.now()
 
+link_list = getAllRecipePageLinks()
+
+# Write links to file in data/links.txt
+with open("data/links.txt", "w") as f:
+	f.writelines(link_list)
+
 #downloadStuff()
-asyncLoad(constructAllRecipePageLinks(count = 30))
+#asyncLoad(constructAllRecipePageLinks(count = 30))
 
 print(datetime.now() - startTime)
